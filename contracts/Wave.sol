0b68e6f5291777ed08a57648f896e22b29f0e276
dkthecoder <dkthecoder@coder.com>
2022-08-22 21:11:11 +0100

    added gettotalwaves, wave function

--- contracts/Wave.sol
+++ contracts/Wave.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Wave{
+    uint256 totalWaves;
+
     constructor(){
         console.log("yo yo, i am a contract that is smart");
+    }
+
+    function wave() public{
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256){
+        console.log("we have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,19 @@
 const { hexStripZeros } = require("ethers/lib/utils")
 
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("Wave");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract Deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
 };
 
 const runMain = async () => {
