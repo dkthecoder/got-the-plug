0c23564194d59c410093862b9899b8e14153efa2
dkthecoder <dkthecoder@coder.com>
2022-08-22 20:09:26 +0100

    first smart contract, update to solidity version

---
+++ contracts/Wave.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Wave{
+    constructor(){
+        console.log("yo yo, i am a contract that is smart");
+    }
+}
